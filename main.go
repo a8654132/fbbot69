604d7aae8b60bac6e5d312ede34f54016bfa2651
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-02 23:10:03 +0800

    love you

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+var mess = &Messenger{}
+
+func main() {
+	port := os.Getenv("PORT")
+	log.Println("Server start in port:", port)
+	mess.VerifyToken = os.Getenv("TOKEN")
+	mess.AccessToken = os.Getenv("TOKEN")
+	log.Println("Bot start in token:", mess.VerifyToken)
+	mess.MessageReceived = MessageReceived
+	http.HandleFunc("/webhook", mess.Handler)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+//MessageReceived :Callback to handle when message received.
+func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
+	// log.Println("event:", event, " opt:", opts, " msg:", msg)
+	profile, err := mess.GetProfile(opts.Sender.ID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你好   , %s %s, %s", profile.FirstName, profile.LastName, msg.Text))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v", resp)
+}
